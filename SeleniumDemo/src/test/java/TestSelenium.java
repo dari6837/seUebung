460a10d7eaed9e600048c4876e4a65872c030c9b
dari6837 <dari6837@a126-20win.inf.fh-flensburg.de>
2014-12-10 11:05:37 +0100

    Tests geupdated

--- SeleniumDemo/src/test/java/TestSelenium.java
+++ SeleniumDemo/src/test/java/TestSelenium.java
@@ -1,9 +1,9 @@
+import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.safari.SafariDriver;
-import org.openqa.selenium.support.PageFactory;
 
 
 public class TestSelenium {
@@ -19,16 +19,21 @@ public class TestSelenium {
 	@Before
 	public void init()
 	{
-		this.driver = new FirefoxDriver();
-		System.out.println("driver()");
+	//	System.out.println("terstsdlöjfnsdlk");
+		//new FirefoxDriver().get("https://blogs.apache.org/");
+		//System.out.println("driver()");
 	}
 	
 	@Test
 	public void startPageTest()
 	{
+		System.out.println("vorTest");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://blogs.apache.org/");
+		assertEquals("Titel pruefen", "blogs", driver.getTitle().substring(0, 5));
 		System.out.println("startPageTest()");
-		StartPage sp = PageFactory.initElements(this.driver, StartPage.class);
-		sp.open("https://blogs.apache.org/");
+		//StartPage sp = PageFactory.initElements(this.driver, StartPage.class);
+		//this.driver.get("https://blogs.apache.org/");
 	}
 
 }
